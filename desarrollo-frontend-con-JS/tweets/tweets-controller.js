555346b6cab876f65e004381494c235c764f7cdb
RamiroCovian <ramiro.covian@gmail.com>
2024-11-17 15:18:50 -0300

    feat: show tweets when button is clicked solution

--- desarrollo-frontend-con-JS/tweets/tweets-controller.js
+++ desarrollo-frontend-con-JS/tweets/tweets-controller.js
@@ -3,9 +3,50 @@
 import { tweets } from "./tweets-model.js";
 import { buildTweet } from "./tweets-view.js";
 
-const tweetsContainer = document.querySelector('#tweets-container')
 
-tweets.forEach(tweet => {
-    const newTweet = buildTweet(tweet);
-    tweetsContainer.appendChild(newTweet)
+function showTweets() {
+    
+    const tweetsContainer = document.querySelector('#tweets-container')
+    tweets.forEach(tweet => {
+        const newTweet = buildTweet(tweet);
+        tweetsContainer.appendChild(newTweet)
+    });
+}
+
+const showTweetsButton = document.querySelector('#show-tweets-button')
+
+showTweetsButton.addEventListener('click', showTweets);
+
+showTweetsButton.addEventListener('click', (event) =>{
+    showTweets();
 });
+
+
+
+
+// const showTweetsButton = document.querySelector('#show-tweets-button')
+// showTweetsButton.getElementById('#show-tweets-button').addEventListener('click', (event)=> {
+//     const tweetsContainer = document.querySelector('#tweets-container')
+    
+//     tweets.forEach(tweet => {
+//         const newTweet = buildTweet(tweet);
+//         tweetsContainer.appendChild(newTweet)
+//     });
+// });
+
+
+
+
+
+
+// // seleccionar boton
+// const btn = document.querySelector("#show-tweets");
+// // agregar evento al boton
+// btn.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     // si se ejecuta el evento - imprimir tweets
+//     tweets.forEach((tweet) => {
+//         const newTweet = buildTweet(tweet);
+//         tweetsContainer.appendChild(newTweet);
+//     });
+// });
